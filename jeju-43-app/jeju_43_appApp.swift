799239ed00927f94af83c991e292c239d9b18262
Tedd Chee <jchee86@gmail.com>
2022-10-18 14:18:26 -0500

    Initial Commit

---
+++ jeju-43-app/jeju_43_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  jeju_43_appApp.swift
+//  jeju-43-app
+//
+//  Created by Tedd Jae Chee on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct jeju_43_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
